6023e361487a4bbaef20af455b7ee343c3c36731
prussell <pamela.russell@gmail.com>
2014-06-09 13:39:41 -0400

    Added skeletons of coordinate space classes

--- src/coordinatespace/CoordinateConverter.java
+++ src/coordinatespace/CoordinateConverter.java
@@ -1,5 +1,20 @@
 package coordinatespace;
 
-public interface CoordinateConverter {
-
+/**
+ * Class to convert coordinates from one coordinate space to another
+ * @author prussell
+ *
+ */
+public class CoordinateConverter {
+	
+	/**
+	 * Coordinate system to convert from
+	 */
+	private CoordinateSpace fromCoordSpace;
+	
+	/**
+	 * Coordinate system to convert to
+	 */
+	private CoordinateSpace toCoordSpace;
+	
 }
--- src/coordinatespace/CoordinateSpace.java
+++ src/coordinatespace/CoordinateSpace.java
@@ -1,5 +1,27 @@
 package coordinatespace;
 
-public class CoordinateSpace {
+import java.util.Collection;
+import java.util.Map;
 
+import annotation.Annotation;
+
+/**
+ * A coordinate space that is a subset of a background genome
+ * @author prussell
+ *
+ */
+public class CoordinateSpace {
+	
+	/**
+	 * The background genome that this coordinate space is a subset of
+	 * Key is chromosome name, value is chromosome length
+	 */
+	private Map<String, Integer> genomeChrSizes;
+	
+	/**
+	 * This coordinate space expressed as annotations on the background genome
+	 * Key is chromosome name, value is the set of annotations on that chromosome
+	 */
+	private Map<String, Collection<Annotation>> coordSpaceOnGenome;
+	
 }
