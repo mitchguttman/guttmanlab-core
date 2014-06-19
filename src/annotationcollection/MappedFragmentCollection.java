004f9327bc18c86ba8b4f4e183271ad757401620
mguttman <mguttman@192.168.0.100>
2014-06-19 09:56:24 -0700

    Removed CoordinateConverter class reference, this will be done natively in the AnnotationCollection

--- src/annotationcollection/MappedFragmentCollection.java
+++ src/annotationcollection/MappedFragmentCollection.java
@@ -1,18 +1,11 @@
 package annotationcollection;
 
-import coordinatespace.CoordinateConverter;
-
 /**
  * A container of fragments mapped to a genome
  * @author prussell
  *
  */
 public interface MappedFragmentCollection extends AnnotationCollection {
-	
-	/**
-	 * @return The coordinate converter for this mapping collection
-	 */
-	public CoordinateConverter getCoordinateConverter();
 	
 	/**
 	 * Write entire dataset to a new file with coordinates converted to other coordinate space according to this object's coordinate converter
