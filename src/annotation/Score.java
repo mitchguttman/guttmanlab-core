1fbadf1c13c3a6ff8d3e011a05696915e6ee1fa5
mguttman <mguttman@192.168.0.100>
2014-06-22 17:12:27 -0700

    Implemented a Window interface that allows storage of the annotation that overlap it.
    Also implemented a MappedFragment interface that stores alignment flags.
    We still need to decide how to do this.

---
+++ src/annotation/Score.java
@@ -0,0 +1,14 @@
+package annotation;
+
+public interface Score {
+
+	/**
+	 * @return Returns a numeric score
+	 */
+	public double getScore();
+	
+	/**
+	 * @return Returns a boolean for whether this score is significant
+	 */
+	public boolean isSignificant();
+}
---
+++ src/annotation/Window.java
@@ -0,0 +1,39 @@
+package annotation;
+
+import java.util.Iterator;
+
+/**
+ * A simple extension of a blocked annotation to account for a window
+ * @author mguttman
+ *
+ */
+public interface Window<T extends Annotation> extends Annotation{
+
+	/**
+	 * The annotation that this Window was made from
+	 * @return The parent annotation that this Window was made from
+	 */
+	public Annotation getParentAnnotation();
+	
+	/**
+	 * A score for this window
+	 * @return A score object for this annotation
+	 */
+	public Score getScore();
+	
+	/**
+	 * Add annotations overlapping this window
+	 * @param annotation An annotation overlapping this window
+	 */
+	public void addAnnotation(T annotation); 
+	
+	/**
+	 * @return The number of annotation stored in this window
+	 */
+	public int getNumberOfAnnotationsInWindow();
+
+	/**
+	 * @return Returns an iterator over the features overlapping the window
+	 */
+	public Iterator<T> getAnnotationsInWindow();
+}
