e23b9906da922ed390c54d7f6a998b5629f6337d
mguttman <mguttman@192.168.0.103>
2014-07-08 17:54:40 -0700

    Basic sequence class

--- src/sequence/Sequence.java
+++ src/sequence/Sequence.java
@@ -1,7 +1,6 @@
 package sequence;
 
 import java.util.Collection;
-
 import annotation.Annotation;
 
 /**
@@ -11,14 +10,72 @@ import annotation.Annotation;
  */
 public class Sequence {
 	
+	private String sequence;
+	private String name;
+	
+	public Sequence(String name, String seq){
+		this.name=name;
+		this.sequence=seq;
+	}
+	
+	public Sequence(String seq){
+		this.sequence=seq;
+	}
+	
 	/**
 	 * @return A new sequence that is the reverse complement of this sequence
 	 */
 	public Sequence reverseComplement() {
-		// TODO
-		throw new UnsupportedOperationException();
+		Sequence tmpSeq = new Sequence(this.sequence);
+		tmpSeq.reverse();
+		return tmpSeq;
 	}
 	
+	
+	private void reverse() {
+		String seqBases = getSequenceBases();
+		String reversedSeq = "";
+		for(int j = seqBases.length() - 1; j >= 0 ; j--) {
+			char c = seqBases.charAt(j);
+			if('c' == c) {
+				reversedSeq+='g';
+			}else if ('C' == c) {
+				reversedSeq+='G';
+			}else if ('G' == c) {
+				reversedSeq+='C';
+			}else if ('g' == c) {
+				reversedSeq+=('c');
+			}else if ('a' == c) {
+				reversedSeq+=('t');
+			}else if ('A' == c) {
+				reversedSeq+=('T');
+			}else if('t' == c) {
+				reversedSeq+=('a');
+			}else if('T' == c) {
+				reversedSeq+=('A');
+			}else if('N'==c){
+				reversedSeq+=('N');
+			}else if('n'==c){
+				reversedSeq+=('n');
+			}else {
+				reversedSeq+=(c);
+			}
+		}
+		
+		this.sequence = reversedSeq;
+	}
+	
+	/**
+	 * @return Return the sequence bases
+	 */
+	public String getSequenceBases() {
+		return this.sequence;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+
 	/**
 	 * Soft mask the specified regions
 	 * Throw an exception if the annotations do not refer to this sequence
@@ -38,7 +95,7 @@ public class Sequence {
 	 */
 	public Sequence hardMask(Collection<Annotation> regions) {
 		// TODO
-		throw new UnsupportedOperationException();
+		throw new UnsupportedOperationException("TODO");
 	}
 	
 }
